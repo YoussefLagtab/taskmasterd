c153e6858643f364dee7b6addd28d2ed8c359f79
Youssef Lagtab <yousseflagtab@gmail.com>
2022-03-02 13:46:47 +0100

    [feat]: add Program->start method
    + spawn a child process with the given command.
    + redirect stdout and stderr.

--- src/program.rs
+++ src/program.rs
@@ -7,60 +7,69 @@ use std::process::{Child, Command};
 pub struct Program {
     pub name: String,
     pub command: String,
-    pub stdout: Option<String>,
-    pub stderr: Option<String>,
+    pub stdout_path: Option<String>,
+    pub stderr_path: Option<String>,
     process: Option<Child>,
 }
 
 impl Program {
     pub fn new(section: String, program_cfg: HashMap<String, Option<String>>) -> Program {
-        let cmd = get_command(&section, program_cfg["command"]);
+        let cmd = get_command(&section, &program_cfg["command"]);
 
         Program {
             name: section,
             command: cmd,
-            stdout: program_cfg["stdout"].clone(),
-            stderr: program_cfg["stderr"].clone(),
+            stdout_path: program_cfg["stdout"].clone(),
+            stderr_path: program_cfg["stderr"].clone(),
             process: None,
         }
     }
 
     pub fn start(mut self) {
         println!("starting {}", self.name);
-        // println!("stdout: {}", self.stdout.unwrap());
-        let mut child = Command::new(self.command);
+        let mut child = Command::new(self.command.clone());
 
+        if (&self.stdout_path).is_some() {
+            let path = self.stdout_path.clone().unwrap();
+            if path.eq("none") {
+                child.stdout(Stdio::null());
+            } else {
+                let file = open_file(path);
+                child.stdout(file);
+            }
+        }
+        if (&self.stderr_path).is_some() {
+            let path = self.stderr_path.clone().unwrap();
+            if path.eq("none") {
+                child.stdout(Stdio::null());
+            } else {
+                let file = open_file(path);
         // child.stdout(self.stdout.unwrap());
         // child.stderr(self.stderr.unwrap());
+                child.stderr(file);
+            }
+        }
         self.process = Some(child.spawn().expect("Failed to spawn child process"));
         println!("{} is started", self.name);
     }
 }
 
 fn get_command(section: &str, command: &Option<String>) -> String {
-    let check_command = || -> Result<String, ()> {
-        let cmd = command.unwrap();
-        if cmd.eq("") {
-            panic!();
+    let cmd = command.clone();
+    if cmd.is_none() || cmd.clone().unwrap().eq("") {
+        println!("Config: command is not given for `{}`", section);
+        exit(1);
         }
-        return Ok(cmd.clone());
-    };
-    match check_command() {
-        Ok(cmd) => cmd.to_string(),
-        Err(e) => panic!("{}: command is not given", section),
-    }
+    cmd.unwrap()
 }
 
-fn open_file(path: Option<String>) -> Option<File> {
-    match path {
-        Some(p) => match File::create(&p) {
+fn open_file(path: String) -> File {
+    match File::create(&path) {
             Err(e) => {
                 println!("Config Error:");
-                println!("=> `{}`: {}", p, e);
+            println!("=> `{}`: {}", path, e);
                 exit(e.raw_os_error().unwrap());
             }
-            Ok(file) => return Some(file),
-        },
-        None => return None,
-    };
+        Ok(file) => return file,
+    }
 }
